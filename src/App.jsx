d2dc718964e09a4ac3e58ea3ce363350690e5d6d
naymurweb <naymurweb@gmail.com>
2023-11-27 23:38:13 +0600

    project create

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-5xl text-red-400'>setup</h1>
+      
+    </div>
+  );
+};
+
+export default App;
